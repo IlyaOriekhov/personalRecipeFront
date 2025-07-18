8a278b3a8c2a02b1274716209e21850b0346d805
Illia <ilya.orekhov@ukr.net>
2025-07-18 20:46:53 +0300

    initial commit, setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-200">
+      <div className="rounded-xl bg-blue-600 p-8 text-center shadow-lg">
+        <h1 className="text-6xl font-bold text-white">Tailwind !</h1>
+        <p className="mt-2 text-lg text-blue-100"></p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
